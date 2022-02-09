832dc252886b98356f50c597691788c6d81e9019
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-02-09 12:01:17 -0700

    ran rails scaffold generator for studio

---
+++ db/migrate/20220209185842_create_studios.rb
@@ -0,0 +1,16 @@
+class CreateStudios < ActiveRecord::Migration[7.0]
+  def change
+    create_table :studios do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+
+      t.timestamps
+    end
+  end
+end
