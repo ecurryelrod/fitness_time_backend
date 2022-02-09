c1cc34b8724008ba100083306b9a6058380293b7
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-02-09 12:26:04 -0700

    added model relationships to studio and teacher models

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,2 +1,4 @@
 class Studio < ApplicationRecord
+    has_many :dance_classes
+    has_many :teachers, through: :dance_classes
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+    has_many :dance_classes
+    has_many :studios, through: :dance_classes
 end
