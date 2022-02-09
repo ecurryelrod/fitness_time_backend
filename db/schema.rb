6680bc21c7db5ec9fea4390ad608f74bbe1c8b62
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-02-09 12:25:25 -0700

    ran rails db:migrate to create schema

---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_02_09_191912) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "dance_classes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.time "time"
+    t.string "level"
+    t.integer "price"
+    t.bigint "teacher_id", null: false
+    t.bigint "studio_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["studio_id"], name: "index_dance_classes_on_studio_id"
+    t.index ["teacher_id"], name: "index_dance_classes_on_teacher_id"
+  end
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "phone"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.integer "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "dance_classes", "studios"
+  add_foreign_key "dance_classes", "teachers"
+end
