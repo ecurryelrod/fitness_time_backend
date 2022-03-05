359c0b77fb815485f0a24e632ea273a3e22c2955
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-05 15:42:40 -0700

    added scope method to dance class model and deleted comments in controller

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,23 +1,20 @@
 class DanceClassesController < ApplicationController
   before_action :set_dance_class, only: %i[ show update destroy ]
 
-  # GET /dance_classes
   def index
     dance_classes = DanceClass.all
+    dance_classes.order_by_date
 
-    render json: DanceClassSerializer.new(dance_classes)
+    render json: DanceClassSerializer.new(dance_classes.order_by_date)
     # render json: dance_classes
   end
 
-  # GET /dance_classes/1
   def show
     render json: DanceClassSerializer.new(@dance_class)
     # render json: @dance_class
   end
 
-  # POST /dance_classes
   def create
-    # dance_class = Studio.dance_classes.build(dance_class_params)
     dance_class = DanceClass.new(dance_class_params)
 
     if dance_class.save
@@ -27,7 +24,6 @@ class DanceClassesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /dance_classes/1
   def update
     if @dance_class.update(dance_class_params)
       render json: DanceClassSerializer.new(@dance_class)
@@ -36,7 +32,6 @@ class DanceClassesController < ApplicationController
     end
   end
 
-  # DELETE /dance_classes/1
   def destroy
     if @dance_class.destroy
       render json: {message: "Dance Class successfully destroyed"}
@@ -46,12 +41,10 @@ class DanceClassesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_dance_class
       @dance_class = DanceClass.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def dance_class_params
       params.require(:dance_class).permit(:title, :description, :date, :start_time, :end_time, :price, :teacher_id, :studio_id, :level_id)
     end
--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -2,4 +2,6 @@ class DanceClass < ApplicationRecord
   belongs_to :teacher
   belongs_to :studio
   belongs_to :level
+
+  scope :order_by_date, -> {order('date')}
 end
